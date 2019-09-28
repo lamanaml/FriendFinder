282f3e30425d9ad775ac05779b0be64720c7a011
lamana <lamanaml@gmail.com>
2019-09-28 19:23:45 -0400

    added js for survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-{
+var friends = {
   "name":"Ahmed",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
@@ -13,4 +13,7 @@
       4,
       1
     ]
-}
+};
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
