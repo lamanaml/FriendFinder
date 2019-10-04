f9dac690333424313d257cdb0d62ccfb3db5ab09
lamana <lamanaml@gmail.com>
2019-10-03 23:49:13 -0400

    updated apiRoutes

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -13,43 +13,50 @@ module.exports = function(app) {
 
           app.post('/api/friends', function (req, res) {
         // loop through all of the possible options
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
+        var thisUser = req.body;
+        var differences = [];
 
-    
-        var userScores = req.body.scores;
-        var userName = req.body.name;
-        var userPhoto = req.body.photo;
-        var totalDifference = 0;
+        // If there is more than one friend to compare to,
+        if (friends.length > 1) {
+            // Step through these potential friends.
+            friends.forEach(function(user) {
+                var totalDifference = 0;
 
-        //loop through the friends data array of objects to get each friends scores
-        for (var i = 0; i < friends.length - 1; i++) {
-            console.log(friends[i].name);
-            totalDifference = 0;
+                // For each answer, compare the answers and add the absolute value of the difference to the total difference.
+                for (var i = 0; i < thisUser.answers.length; i++) {
+                    var otherAnswer = user.answers[i];
+                    var thisAnswer = thisUser.answers[i];
+                    var difference = +otherAnswer - +thisAnswer;
+                    totalDifference += Math.abs(difference);
+                }
 
-            //loop through that friends score and the users score and calculate the absolute difference between the two and push that to the total difference variable set above
-            for (var j = 0; j < 10; j++) {
-                // We calculate the difference between the scores and sum them into the totalDifference
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-                // If the sum of differences is less then the differences of the current "best match"
-                if (totalDifference <= bestMatch.friendDifference) {
+                differences.push(totalDifference);
+            });
 
-                    // Reset the bestMatch to be the new friend. 
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
-                    bestMatch.friendDifference = totalDifference;
+            // Find the minimum difference score.
+            var minimumDifference = Math.min.apply(null, differences);
+
+            // Since there may be more than one potential friend with that score, create an array.
+            var bestMatches = [];
+
+            // For each item in differences, if it is equal to the minimumDifference, add the corresponding friendData to the bestMatches array.
+            for (var i = 0; i < differences.length; i++) {
+                if (differences[i] === minimumDifference) {
+                    bestMatches.push(friends);
                 }
             }
+
+            // Then send bestMatches to the client.
+            res.json(bestMatches);
+        // If there is only one friend to compare to, skip all that work and just send back that friend.
+        } else {
+            res.json(friends);
         }
 
-        // The push method use to save user's data to the database
-        friends.push(req.body);
+        // Once you're done comparing, add the new user to the potential friends data.
+        friends.push(thisUser);
 
-        //The res.json method will return a JSON data with the user's match which was looped through frieds data array. 
-        res.json(bestMatch);
-    });
-     
-};
+    })
+}
+
+
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require('path');
 
 var app = express();
-var PORT = process.env.PORT || 8081;
+var PORT = process.env.PORT || 8082;
  
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
