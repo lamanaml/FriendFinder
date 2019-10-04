77e581adab25a50371087284fb9f45848c9a9f72
lamana <lamanaml@gmail.com>
2019-10-03 22:36:31 -0400

    corrected routes

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,9 +1,61 @@
-var friendData = require("../data/friends");
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
-    app.get("/api/friends", function(req, res){
-        res.json(friendData)
+
+
+    // Sets the get for the api/friends route
+    app.get('/api/friends', function(req, res) {
+        res.json(friends);
+        console.log(friends);
+       
     });
+
+
+     app.post('/api/friends', function(req, res) {
+
+     
+    	//	Set variables only needed for the post
+        var difference = 40;
+        var matchName = '';
+        var matchPhoto = '';
+
+   // For-each loop to go through the data in friends.js to find a match
+      for (var j = 0; j < friends.length; j++) {
     
-   
+    // Variables for comparing matches
+            var matchedScoresArray = [];
+            var totalDifference 
+
+    // Function to assist in the addition reduce() below
+          function add(total, num) {
+               return total + num;
+           }
+      }
+           
+    // new value to the matchedScoresArray
+    //  for (var i = 0; i < friends.scores.length; i++) {
+    //  matchedScoresArray.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(friends.scores[i])));
+    //  console.log(newFriends.scores)
+
+     }
+
+            
+    
+    //     // Once the cycle is complete, the match with the least difference will remain,
+    //    // and that data will be sent as a json object back to the client
+    //     res.json({
+    //         name: matchName,
+    //         photo: matchPhoto
+
+    //     });
+    //             })
+    //     });
+     
+
+    //     // This adds the new users sent data object to friends.js
+    //     friends.push(req.body);
+    
+
+ })
 }
+
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require("express");
+var path = require('path');
+
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8081;
  
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+app.use(express.static(__dirname + '/app/public'));
 
  //set up routes
 require('./app/routes/apiRoutes.js')(app);
